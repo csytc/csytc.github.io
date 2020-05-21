9e62f8beb15e7cc1cae3f980373d6194390ce4ab
csy <125241477@qq.com>
2020-05-21 20:13:48 +0800

    <task

--- 39Vuex/vuex/src/App.vue
+++ 39Vuex/vuex/src/App.vue
@@ -2,8 +2,26 @@
   <div>
     <h2>{{message}}</h2>
     <hellovuex/>
+    <h2>------添加state数据相关内容-------</h2>
+    <button @click="updated">添加数据</button>
+    <h2>{{$store.state.Info}}</h2>
     <button @click="addition">+</button>
     <button @click="subtract">-</button>
+    <button @click="addCount(5)">+5</button>
+    <button @click="addCount(10)">+10</button>
+
+    <h2>---------App内容：getters相关信息---------</h2>
+    <h2>{{$store.getters.power}}</h2>
+    <h2>{{$store.getters.filters}}</h2>
+    <h2>{{$store.getters.more20length}}</h2>
+    <h2>{{$store.getters.moreAgestu(18)}}</h2>
+
+    <h2>-----------App内容：modules的测试----------</h2>
+    <h2>{{$store.state.a.name}}</h2>
+    <button @click="change">修改module里的name</button>
+    <h2>{{$store.getters.fullName}}</h2>
+    <h2>{{$store.getters.fullName2}}</h2>
+    <button @click="asyncUpdateName">异步修改名字</button>
   </div>
 </template>
 
@@ -22,6 +40,37 @@ export default {
       },
       subtract(){
         this.$store.commit("decrement")
+      },
+      addCount(num){
+        //1.普通提交风格
+        this.$store.commit("incrementCount",num)
+        //2.特殊提交风格
+      //  this.$store.commit({
+      //    type:"incrementCount",
+      //    count:num
+      //  })
+      },
+      updated () {
+        //同步的修改方法
+        //this.$store.commit("updateInfo")  ;
+
+        //异步的修改方法，要经过actions
+        // this.$store.dispatch("aUpdateInfo",{
+        //   message:"我是携带的信息",
+        //   success:()=>{
+        //     console.log("里面已经完成了")
+        //   }
+        // })
+        this.$store.dispatch("aUpdateInfo","我是携带的信息").then((res)=>{
+          console.log("里面完成了提交")
+          console.log(res);
+        })
+      },
+      change(){
+        this.$store.commit("updatename","ccc");
+      },
+      asyncUpdateName (){
+        this.$store.dispatch("updateName")
       }
   }
     
--- 39Vuex/vuex/src/store/index.js
+++ 39Vuex/vuex/src/store/index.js
@@ -2,10 +2,52 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 
 Vue.use(Vuex)
+    //创建module的对象
+const moduleA = {
+    state: {
+        name: "张三"
+    },
+    mutations: {
+        updatename(state, payload) {
+            state.name = payload
+        }
+    },
+    getters: {
+        fullName(state) {
+            return state.name + "111"
+        },
+        //模块里面可以传入第三个元素
+        fullName2(state, getters, rootState) {
+            return getters.fullName + rootState.counter
+        }
+    },
+    actions: {
+        updateName(context) {
+            //模块里面调用commit只会调用自己的mutations
+            setTimeout(() => {
+                context.commit("updatename", "wangwu")
+            })
+        }
+    }
+}
+
+
+
+
 
 export default new Vuex.Store({
     state: {
-        counter: 1000
+        counter: 1000,
+        students: [
+            { id: 110, name: "aaa", age: 18 },
+            { id: 111, name: "bbb", age: 22 },
+            { id: 112, name: "ccc", age: 34 },
+            { id: 113, name: "ddd", age: 10 }
+        ],
+        Info: {
+            name: "csy",
+            age: 18
+        }
     },
     mutations: {
         //方法，参数默认是state对象,以后如果要修改state里面的数据大多是需要mutations进行修改
@@ -14,8 +56,67 @@ export default new Vuex.Store({
         },
         decrement(state) {
             state.counter--;
+        },
+        incrementCount(state, num) {
+            state.counter += num
+                //如果说以特殊提交风格的话，那么此时的num传过来的就是一个对象
+                //state.counter += num.count
+        },
+        updateInfo(state) {
+            state.Info.name = "abc"
+                //响应式添加数据
+                // Vue.set(state.Info, "address", "hefei");
+                //响应式删除数据
+                // Vue.delete(state.Info,"age")
         }
     },
-    actions: {},
-    modules: {}
+    actions: {
+        //默认参数context上下文
+        // aUpdateInfo(context, payload) {
+        //     setTimeout(() => {
+        //         context.commit("updateInfo")
+        //         console.log(payload.message);
+        //         payload.success()
+        //     }, 2000)
+        // }
+
+        //使用promise写法
+        aUpdateInfo(context, payload) {
+            return new Promise((resolve, reject) => {
+                setTimeout(() => {
+                    context.commit("updateInfo")
+                    console.log(payload)
+                    resolve("111")
+                }, 2000)
+            })
+        }
+    },
+    modules: {
+        a: moduleA,
+        b: {
+            state: {},
+            mutations: {},
+            actions: {},
+            getters: {}
+        }
+    },
+    getters: {
+        //参数默认是state对象,以后如果要修改state里面的数据大多是需要mutations进行修改
+        power(state) {
+            return state.counter * state.counter
+        },
+        filters(state) {
+            return state.students.filter(s => s.age >= 20)
+        },
+        //获取年龄大于20的人的个数
+        more20length(state, getters) { //第二个参数为getters对象
+            return getters.filters.length
+        },
+        //获取年龄大于任意给定的值
+        moreAgestu(state) {
+            return function(age) {
+                return state.students.filter(s => s.age >= age)
+            }
+        }
+    }
 })
